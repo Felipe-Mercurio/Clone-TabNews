d90f1c4b8b46c95d49cb65b35ebc03c34df52542
Felipe Mercurio <95919509+Felipe-Mercurio@users.noreply.github.com>
2023-08-17 19:26:48 +0000

    adicona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home(){
+    return <div>
+        <h1>Amor eu te amo!</h1>
+        <h2>Hoje fazemos 11 meses!</h2>
+        <br></br>
+        De: Felipe
+        <br></br>
+        Para: Rafaela
+    </div>
+}
+
+export default Home;
