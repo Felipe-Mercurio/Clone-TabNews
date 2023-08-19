e88e4efa227fec943ec4a270ad0d52deb2ea0921
Felipe Mercurio <95919509+Felipe-Mercurio@users.noreply.github.com>
2023-08-19 22:26:14 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
-function Home(){
-    return <div>
-        <h1>Amor eu te amo!</h1>
-        <h2>Hoje fazemos 11 meses!</h2>
-        <br></br>
-        De: Felipe
-        <br></br>
-        Para: Rafaela
+function Home() {
+  return (
+    <div>
+      <h1>Amor eu te amo!</h1>
+      <h2>Hoje fazemos 11 meses!</h2>
+      <br></br>
+      De: Felipe
+      <br></br>
+      Para: Rafaela
     </div>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste");
 }
 
 export default Home;
